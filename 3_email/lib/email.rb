6198c690006295b86d991b5880e41235b4b52549
andooown <andooown.dev@gmail.com>
2019-04-10 17:34:34 +0900

    Create DotAtomVerifier

--- 3_email/lib/email.rb
+++ 3_email/lib/email.rb
@@ -1,7 +1,72 @@
 class EmailVerifier
 
   def execute(email)
-    true
+    splited = email.split('@')
+    if splited.size == 2
+      raise ArgumentError
+    end
+    domain, local = splited
+  end
+
+
+end
+
+class EmailLocalVerifier
+
+  def initialize
+    @verifiers = [
+      DotAtomVerifier.new,
+      QuatedStringVerifier.new
+    ]
+  end
+
+  def execute(local)
+    @verifiers.any {|verifier| verifier.execute(local) }
+  end
+
+
+end
+
+class DotAtomVerifier
+
+  def execute(local)
+    [
+      ld1(local),
+      ld2(local),
+      ld3(local),
+      ld4(local),
+      ld5(local)
+    ].all?
+  end
+
+  def ld1(local)
+    except = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'*+-/=?^_`{|}~."
+    
+    local.chars.to_a.all? {|c| except.include?(c) }
+  end
+
+  def ld2(local)
+    !local.start_with?('.')
+  end
+
+  def ld3(local)
+    !local.end_with?('.')
+  end
+
+  def ld4(local)
+    !local.include?('..')
+  end
+
+  def ld5(local)
+    local.size >= 1
+  end
+
+end
+
+class QuatedStringVerifier
+
+  def execute(local)
+    false
   end
 
 end
--- 3_email/spec/email_spec.rb
+++ 3_email/spec/email_spec.rb
@@ -8,8 +8,44 @@ class EmailVerifierTest < Minitest::Test
   end
 
   def test_email
-    assert @verifier.execute('abc@example.com')
-    assert !@verifier.execute('a..bc@example.com')
+    # assert @verifier.execute('abc@example.com')
+    # assert !@verifier.execute('a..bc@example.com')
+
+    # LD5
+    # assert !@verifier.execute('@example.com')
+  end
+
+end
+
+class DotAtomVerifierTest < Minitest::Test
+
+  def setup
+    @verifier = DotAtomVerifier.new
+  end
+
+  def test_ld1
+    assert @verifier.ld1("fodFJODN5345!#$%&'*+-/=?^_`{|}~.")
+    assert !@verifier.ld1('ふぁｄｆひｄｆはｓｆ')
+  end
+
+  def test_ld2
+    assert @verifier.ld2('fasldkfjaod')
+    assert !@verifier.ld2('.fsdfa')
+  end
+
+  def test_ld3
+    assert @verifier.ld3('fasldkfjaod')
+    assert !@verifier.ld3('fsdfa.')
+  end
+
+  def test_ld4
+    assert @verifier.ld4('fasldkfjaod')
+    assert !@verifier.ld4('a..bc')
+  end
+
+  def test_ld5
+    assert @verifier.ld5('fasldkfjaod')
+    assert !@verifier.ld5('')
   end
 
 end
